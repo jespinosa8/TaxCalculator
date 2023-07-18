bba2fb20afefaef9dddb904ed852ba127953a75c
jarsenau11 <jarsenau@asu.edu>
2023-07-18 10:28:42 -0700

    Initial commit

---
+++ tax_prep_app_backend/src/main/java/com/project2/tax_prep_app_backend/TaxPrepAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project2.tax_prep_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxPrepAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxPrepAppBackendApplication.class, args);
+	}
+
+}
---
+++ tax_prep_app_backend/src/test/java/com/project2/tax_prep_app_backend/TaxPrepAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2.tax_prep_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxPrepAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
