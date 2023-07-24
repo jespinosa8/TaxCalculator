f8588c0067554fb19f6b4d279a19dbfb6acaa77b
unknown <acsmithwt@gmail.com>
2023-07-24 18:18:20 -0400

    SQL updates

--- SqlFiles/tax_prep_tables.sql
+++ SqlFiles/tax_prep_tables.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS users, user_detail, tax_filing, form_w2, form_1099;
+
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
@@ -8,13 +10,13 @@ CREATE TABLE users (
 CREATE TABLE user_detail (
   user_detail_id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(user_id),
-  ssn NUMERIC(9),
-  first_name VARCHAR(50),
+  ssn NUMERIC(9) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
   middle_name VARCHAR(50),
-  last_name VARCHAR(50),
+  last_name VARCHAR(50) NOT NULL,
   email VARCHAR(100),
   dob DATE,
-  street1 VARCHAR(50),
+  street1 VARCHAR(50) NOT NULL,
   street2 VARCHAR(50),
   city VARCHAR(50),
   state VARCHAR(50),
@@ -35,7 +37,7 @@ CREATE TABLE form_w2 (
   form_w2_id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(user_id),
   tax_filing_id INTEGER REFERENCES tax_filing(tax_filing_id),
-  income NUMERIC(8,2),
+--   income NUMERIC(8,2),
   ein NUMERIC(9),
   employer_street1 VARCHAR(50),
   employer_street2 VARCHAR(50),
@@ -43,11 +45,11 @@ CREATE TABLE form_w2 (
   employer_state VARCHAR(50),
   employer_zip NUMERIC(10),
   wages_and_tips NUMERIC(8,2),
-  total_compensation NUMERIC(8,2),
-  ss_wages NUMERIC(8,2),
+--   total_compensation NUMERIC(8,2),
+--   ss_wages NUMERIC(8,2),
   ss_withheld NUMERIC(8,2),
   taxes_withheld NUMERIC(8,2),
-  medicare_wages NUMERIC(8,2),
+--   medicare_wages NUMERIC(8,2),
   medicare_withheld NUMERIC(8,2)
 );
 
@@ -55,12 +57,12 @@ CREATE TABLE form_1099 (
   form_1099_id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(user_id),
   tax_filing_id INTEGER REFERENCES tax_filing(tax_filing_id),
-  payer_fin NUMERIC(9),
+  payer_tin NUMERIC(9),
   payer_street1 VARCHAR(50),
   payer_street2 VARCHAR(50),
   payer_city VARCHAR(50),
   payer_state VARCHAR(50),
   payer_zip NUMERIC(10),
-  recipient_ssn_ein NUMERIC(9),
+  recipient_tin NUMERIC(9),
   total_compensation NUMERIC(8,2)
 );
