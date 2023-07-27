93f7e29081f3d429a1d042120dd3d5706e2d6f38
unknown <acsmithwt@gmail.com>
2023-07-27 15:28:56 -0400

    i18 working and translated primary nav

--- tax_prep_app_frontend/src/App.tsx
+++ tax_prep_app_frontend/src/App.tsx
@@ -10,26 +10,39 @@ import PersonalInformation from './pages/PersonalInformation';
 import TaxFiling from './pages/TaxFiling';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
 
 function App() {
+
+    const { t, i18n } = useTranslation();
+
+    useEffect(() => {
+        const lng = navigator.language;
+        i18n.changeLanguage(lng);
+    }, [])
+
+    const lng = navigator.language;
 
     const navItems = [
 
         // creating a list of react-router-dom Links to pass to our navbar
-        <Link to='/'>Home</Link>,
-        <Link to='/tax-summary'>Tax Summary</Link>,
-        <Link to='/tax-filing'>Add General Tax Information</Link>,
-        <Link to='/add-w2'>Add W-2</Link>,
-        <Link to='/add-1099'>Add 1099</Link>,
-        <Link to='/personal-info'>Personal Information</Link>,
-        <Link to='/login'>Log Out</Link>
+        <Link to='/'>{t('link.home')}</Link>,
+        <Link to='/tax-summary'>{t('link.taxSummary')}</Link>,
+        <Link to='/tax-filing'>{t('link.generalTaxInformation')}</Link>,
+        <Link to='/add-w2'>{t('link.addW2')}</Link>,
+        <Link to='/add-1099'>{t('link.add1099')}</Link>,
+        <Link to='/personal-info'>{t('link.personalInformation')}</Link>,
+        <Link to='/login'>{t('link.logOut')}</Link>,
+        // <div>Browser Language: {lng}</div>
     ];
 
     return (
         <>
             <BrowserRouter basename='/'>
                 <Header id="nav-bar-container">
-                    <PrimaryNav className='bg-primary-lighter' items={navItems}/>
+                    <PrimaryNav className='bg-primary-lighter' items={navItems}>
+                        </PrimaryNav>
                 </Header>
 
                 <Routes>
