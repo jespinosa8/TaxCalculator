25d8594e6e904659ecb078c562b5cee354a49b75
jarsenau11 <jarsenau@asu.edu>
2023-07-28 17:51:03 -0700

    progress on general info page

--- tax_prep_app_frontend/src/pages/TaxFiling.tsx
+++ tax_prep_app_frontend/src/pages/TaxFiling.tsx
@@ -1,10 +1,31 @@
+import React, { useState } from "react"
+import GeneralTaxInformation from "../components/taxFiling/GeneralTaxInformation"
 
+export default function TaxFiling() {
+    const [taxFiling, setTaxFiling] = useState({
+      married: false,
+      dependents: 0
+    })
+    const [currentPage, setCurrentPage] = useState(1)
 
+    const handleMarriedChange = (married: boolean) => {
+      setTaxFiling((prev) => ({ ...prev, married: married }))
+    }
 
-export default function TaxFiling () {
+    const handleDependentsChange = (event: any) => {
+      setTaxFiling((prev) => ({ ...prev, dependents: event.target.value }))
 
+    }
+
+    const handleGeneralTaxInfoNextButton = () => {
+        setCurrentPage(2)
+    }
+
+    // maybe add some kind of progress tracker at the top separately that will show lit up circles with checkmarks depending on what step the user is on
     return (
-      <>
-      </>
+        <>
+          {currentPage == 1 && (<GeneralTaxInformation taxFiling={taxFiling} handleDependentChange={handleDependentsChange} handleMarried={handleMarriedChange} handleNextButtonClick={handleGeneralTaxInfoNextButton}></GeneralTaxInformation>)}
+        
+        </>
     )
-  }
+}
